e78f8ca981f8480fccfd02b8f9e31ad6b7117ac2
vthriller <unixway.drive+git@gmail.com>
2020-07-13 19:21:57 +0300

    enable more warnings

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,9 @@
 #![warn(
 	missing_debug_implementations,
-	// TODO
-	//missing_docs,
-	// XXX how to limit this to C-like enums? I'd like to #[derive(Copy)] them
-	// see also https://github.com/rust-lang-nursery/rust-clippy/issues/2222
-	//missing_copy_implementations,
+	missing_docs,
 	trivial_casts,
 	trivial_numeric_casts,
-	// XXX this crate is all about unsafe code, but we should probably limit that to certain modules
-	//unsafe_code,
+	unsafe_code,
 	unstable_features,
 	unused_import_braces,
 	unused_qualifications,
